4aafa18ac61834bd7034bdd784e65d31fe8ef74a
CheongYeeMing <e0526001@u.nus.edu>
2021-06-16 16:53:25 +0800

    Bug Fix

--- Assets/Scripts/MobHealth.cs
+++ Assets/Scripts/MobHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MobHealth : MonoBehaviour, Health
 {
@@ -10,7 +11,6 @@ public class MobHealth : MonoBehaviour, Health
     [SerializeField] public Transform DamagePopup;
     [SerializeField] public Transform HealingPopup;
     [SerializeField] public float maxHealth;
-    
 
     public float currentHealth;
     public float regenTimer; // Default 10%maxHP/second
