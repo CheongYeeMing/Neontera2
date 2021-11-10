e1dcd77053ca9e66889da472f7bb025fe13735d6
CheongYeeMing <e0526001@u.nus.edu>
2021-11-10 11:46:32 +0800

    Update Animation interface.

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -1,9 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+/// <summary>
+/// This interface controls the Animation of all GameObjects.
+/// </summary>
 public interface Animation
-{
+{   
+    /// <summary>
+    /// This method is called when GameObject is initialised.
+    /// </summary>
     public void Start();
+
+    /// <summary>
+    /// This method has to be implemented by every GameObject
+    /// that has some form of animation.
+    /// </summary>
+    /// <param name="newState"></param>
     public void ChangeAnimationState(string newState);
 }
