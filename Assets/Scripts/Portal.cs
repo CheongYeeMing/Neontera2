cd7f86237653aa69b6dc8b5d986f8fe000f33953
CheongYeeMing <e0526001@u.nus.edu>
2021-12-17 18:38:41 +0800

    Add `POSITION_Y_OFFSET` variable.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    private const float POSITION_Y_OFFSET = -1.2f;
     [SerializeField] GameObject PortalNameTag;
     [SerializeField] public Portal Destination;
     [SerializeField] public string Location;
@@ -18,7 +19,7 @@ public class Portal : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        PortalNameTag.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.2f);
+        PortalNameTag.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + POSITION_Y_OFFSET);
         if (isActivated)
         {
             animator.SetTrigger("Activate");
