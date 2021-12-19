603bf2912dc7cd46c15d8bbfdf695b279683059e
CheongYeeMing <e0526001@u.nus.edu>
2021-12-19 19:45:40 +0800

    Remove commented code.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -16,20 +16,12 @@ public class GameOver : MonoBehaviour
         {
             character.GetComponent<ParallaxBackgroundManager>().Respawn("Intro", character.gameObject, RespawnPointIntro.position);
             RespawnMonologueIntro.gameObject.SetActive(true);
-            //character.transform.position = RespawnPointIntro.position;
         }
         else
         {
-
-            //character.GetComponent<ParallaxBackgroundManager>().OffBackground();
-            //character.GetComponent<ParallaxBackgroundManager>().SetBackground("Town");
-            //character.GetComponent<ParallaxBackgroundManager>().Teleport("Town", character.gameObject, RespawnPointTown.position);'
             character.GetComponent<ParallaxBackgroundManager>().Respawn("Town", character.gameObject, RespawnPointTown.position);
             RespawnMonologueTown.gameObject.SetActive(true);
-            //character.transform.position = RespawnPointTown.position;
         }
-        //character.GetComponent<CharacterHealth>().Revive();
-        //character.GetComponent<CharacterWallet>().MinusGold(character.GetComponent<CharacterWallet>().GetGoldAmount() / 10);
         gameObject.SetActive(false);
     }
 }
