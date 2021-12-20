6390032de71639f4e402c57bb7ec1085991e7c00
CheongYeeMing <e0526001@u.nus.edu>
2021-12-20 19:24:07 +0800

    Add `ACTIVATE_PORTAL` variable.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     private const float PORTAL_NAME_TAG_POSITION_Y_OFFSET = -1.2f;
+    private const string ACTIVATE_PORTAL = "Activate";
+    private const string DEACTIVATE_PORTAL = "Deactivate";
 
     [SerializeField] GameObject portalNameTag;
     [SerializeField] public Portal destinationPortal;
