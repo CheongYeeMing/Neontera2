caed52abae43402e3d5fba2b7e97232a719de9bc
CheongYeeMing <e0526001@u.nus.edu>
2021-12-22 15:22:49 +0800

    Correct variable naming convention.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,22 +5,22 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    [SerializeField] private Transform RespawnPointIntro;
-    [SerializeField] private Transform RespawnPointTown;
-    [SerializeField] private Monologue RespawnMonologueIntro;
-    [SerializeField] private Monologue RespawnMonologueTown;
+    [SerializeField] private Transform respawnPointIntro;
+    [SerializeField] private Transform respawnPointTown;
+    [SerializeField] private Monologue respawnMonologueIntro;
+    [SerializeField] private Monologue respawnMonologueTown;
     public void Respawn(Character character)
     {
         
         if (character.GetComponent<CharacterMovement>().location == "Intro")
         {
-            character.GetComponent<ParallaxBackgroundManager>().Respawn("Intro", character.gameObject, RespawnPointIntro.position);
-            RespawnMonologueIntro.gameObject.SetActive(true);
+            character.GetComponent<ParallaxBackgroundManager>().Respawn("Intro", character.gameObject, respawnPointIntro.position);
+            respawnMonologueIntro.gameObject.SetActive(true);
         }
         else
         {
-            character.GetComponent<ParallaxBackgroundManager>().Respawn("Town", character.gameObject, RespawnPointTown.position);
-            RespawnMonologueTown.gameObject.SetActive(true);
+            character.GetComponent<ParallaxBackgroundManager>().Respawn("Town", character.gameObject, respawnPointTown.position);
+            respawnMonologueTown.gameObject.SetActive(true);
         }
         gameObject.SetActive(false);
     }
