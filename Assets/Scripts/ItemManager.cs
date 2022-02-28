287155d12a7cbcb1b3f6567e30ba9aca0af85e5e
CheongYeeMing <e0526001@u.nus.edu>
2022-02-28 17:43:45 +0800

    Fix check style issue

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
+    // List if In-Game Items on the ground
     [SerializeField] List<GameObject> Item;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         for (int i = 0; i < Data.Item.Count; i++)
         {
             if (Data.Item[i] == 0)
+            {
                 Item[i].gameObject.SetActive(false);
+            }
         }
     }
 
+    // Saves the state of Items on the ground
+    // Whether they have been picked up before
     public void Save()
     {
         Data.Item.Clear();
