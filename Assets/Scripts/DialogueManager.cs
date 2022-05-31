f12641e32e536d03dea86cd0783e3a91239277fd
CheongYeeMing <e0526001@u.nus.edu>
2022-05-31 13:24:21 +0800

    Fix styling issue

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -85,7 +85,6 @@ public class DialogueManager : MonoBehaviour
             {
                 CompleteSentence();
                 return;
-
             }
             if (npc.Sequences[npc.sequenceNumber].isStory)
             {
