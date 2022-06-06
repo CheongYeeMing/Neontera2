f4e6de5dbef6073d654966650e3445aa24daf8c8
CheongYeeMing <e0526001@u.nus.edu>
2022-06-06 23:12:11 +0800

    Remove unused directories

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseCursor : MonoBehaviour
-{   public GameObject trialEffect;
+{   
+    public GameObject trialEffect;
     public Texture2D cursorArrow;
+
     public float timeBtwSpawn = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
