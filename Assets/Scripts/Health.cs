13591594f9e7f869ae417ba63c87997f75bfe14f
CheongYeeMing <e0526001@u.nus.edu>
2022-06-07 21:52:08 +0800

    Clear branch of useless items

--- Assets/Scripts/Health.cs
+++
@@ -1,6 +0,0 @@
-public interface Health
-{
-    public void TakeDamage(float damage);
-
-    public void Die();
-}
--- Assets/Scripts/SupremeLeaderAnimation.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SupremeLeaderAnimation : BossAnimation
-{
-    protected const string SUPREME_LEADER_DEATHRAY = "DeathRay";
-    protected const string SUPREME_LEADER_LASER = "Shoot";
-}
