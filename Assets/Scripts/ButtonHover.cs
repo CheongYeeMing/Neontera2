7c686b21371cd89666195f78242b8d1239861538
CheongYeeMing <e0526001@u.nus.edu>
2022-07-24 03:09:43 +0800

    Update Main Menu UI

---
+++ Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator buttonAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();
+        HoverOff();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HoverOn();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HoverOff();
+    }
+
+    public void HoverOn()
+    {
+        buttonAnimator.Play("HoverOn");
+    }
+
+    public void HoverOff()
+    {
+        buttonAnimator.Play("HoverOff");
+    }
+}
