7abe5531e23f72b67b82dc9e08fec90f7423cba4
CheongYeeMing <e0526001@u.nus.edu>
2022-07-30 22:07:01 +0800

    Add animation for Main menu

--- Assets/Scripts/PlayMenu.cs
+++ Assets/Scripts/PlayMenu.cs
@@ -8,45 +8,55 @@ public class PlayMenu : MonoBehaviour
     private const float MAXIMUM_PROGRESS = 100f;
     private const string PLAYERPREF_KEY_LEVEL = "level";
 
-    [SerializeField] GameObject StartMenu;
+    [SerializeField] GameObject MainMenu;
     [SerializeField] GameObject StartButton;
+    [SerializeField] GameObject SettingsButton;
+    [SerializeField] GameObject QuitButton;
+    [SerializeField] GameObject LoadGameButton;
+    [SerializeField] GameObject NewGameButton;
+    [SerializeField] GameObject BackButton;
+    [SerializeField] GameObject SettingsBackButton;
     [SerializeField] GameObject NoSavedData;
     [SerializeField] GameObject OverwriteData;
     [SerializeField] GameObject LoadingScreen;
     [SerializeField] Slider slider;
     [SerializeField] Text progressText;
 
-    private AudioSource buttonClick;
+    [SerializeField] GameObject MainMenuButtons;
+    [SerializeField] GameObject SettingsMenuButtons;
+    [SerializeField] GameObject PlayMenuButtons;
+
+    private AudioManager audioManager;
 
     private void Start()
     {
-        buttonClick = StartButton.GetComponent<AudioSource>();
+        audioManager = FindObjectOfType<AudioManager>();
+        audioManager.PlayMusic("Main Menu");
     }
 
     public void CloseSavedData()
     {
-        StartButton.GetComponent<AudioSource>().Stop();
-        StartButton.GetComponent<AudioSource>().Play();
+        PlayButtonClickSound();
         NoSavedData.SetActive(false);
     }
 
     public void DoNotOverwriteData()
     {
-        StartButton.GetComponent<AudioSource>().Stop();
-        StartButton.GetComponent<AudioSource>().Play();
+        PlayButtonClickSound();
         OverwriteData.SetActive(false);
     }
 
     public void PlayGame()
     {
+        audioManager.MainMenuSave();
         StartCoroutine(LoadAsynchronously());
     }
 
     IEnumerator LoadAsynchronously()
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(1); // Proceed to Intro Level scene
-
         LoadingScreen.SetActive(true);
+        yield return new WaitForSeconds(0.01f);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1); // Proceed to Intro Level scene
 
         while (!operation.isDone)
         {
@@ -62,7 +72,11 @@ public class PlayMenu : MonoBehaviour
     public void Play()
     {
         PlayButtonClickSound();
-        StartMenu.SetActive(true);
+        PlayMenuButtons.SetActive(true);
+        MainMenuButtons.SetActive(false);
+        LoadGameButton.GetComponent<ButtonHover>().HoverOff();
+        NewGameButton.GetComponent<ButtonHover>().HoverOff();
+        BackButton.GetComponent<ButtonHover>().HoverOff();
     }
 
     public void LoadGame()
@@ -100,7 +114,11 @@ public class PlayMenu : MonoBehaviour
     public void ReturnToMainMenu()
     {
         PlayButtonClickSound();
-        StartMenu.SetActive(false);
+        PlayMenuButtons.SetActive(false);
+        MainMenuButtons.SetActive(true);
+        StartButton.GetComponent<ButtonHover>().HoverOff();
+        SettingsButton.GetComponent<ButtonHover>().HoverOff();
+        QuitButton.GetComponent<ButtonHover>().HoverOff();
     }
 
     public void ExitGame()
@@ -108,10 +126,25 @@ public class PlayMenu : MonoBehaviour
         PlayButtonClickSound();
         Application.Quit();
     }
+    public void Settings()
+    {
+        PlayButtonClickSound();
+        SettingsMenuButtons.SetActive(true);
+        MainMenuButtons.SetActive(false);
+        SettingsBackButton.GetComponent<ButtonHover>().HoverOff();
+    }
+    public void SettingsBack()
+    {
+        PlayButtonClickSound();
+        SettingsMenuButtons.SetActive(false);
+        MainMenuButtons.SetActive(true);
+        StartButton.GetComponent<ButtonHover>().HoverOff();
+        SettingsButton.GetComponent<ButtonHover>().HoverOff();
+        QuitButton.GetComponent<ButtonHover>().HoverOff();
+    }
 
     private void PlayButtonClickSound()
     {
-        buttonClick.Stop();
-        buttonClick.Play();
+        audioManager.PlayEffect("RetroClick");
     }
 }
